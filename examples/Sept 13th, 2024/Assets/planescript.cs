3d7bb29834e8309cb2ca61eac882243fb8ed6bbf
eve2024 <144040969+eve2024@users.noreply.github.com>
2024-09-17 12:09:36 -0400

    build game
    build game for the web for the RGB

--- examples/Sept 13th, 2024/Assets/planescript.cs
+++ examples/Sept 13th, 2024/Assets/planescript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class planescript : MonoBehaviour
 {
 
-    float fowardSpeed = 0.01f;
+    float forwardSpeed = 0.01f;
+    float xRotationSpeed = 0.02f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,21 @@ public class planescript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.posistion +=  transform.forward * transform.fowardSpeed;
+        float vAxis = Input.GetAxis("Vertical");
+        float hAxis = Input.GetAxis("Horizontal");
+
+
+        transform.Rotate(vAxis, hAxis, 0, Space.Self);
+        
+        transform.posistion +=  transform.forward * forwardSpeed;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.CompareTage("collectable"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
