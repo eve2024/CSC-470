2bc7dce7d823245b127d323b14dae116361674c7
eve2024 <144040969+eve2024@users.noreply.github.com>
2024-09-18 16:53:27 -0400

    Camera View
    Added the camera to follow the plane as it moves

--- assigments/Flight Simulator/Assets/PlaneScript.cs
+++ assigments/Flight Simulator/Assets/PlaneScript.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlaneScript : MonoBehaviour
 {
+
+    public GameObject cameraObject;
     // These variables will control how the plane moves
-    float forwardSpeed = 0.1f;
-    float xRotationSpeed = 0.2f; 
-    float yRotationSpeed = 0.2f; 
+    float forwardSpeed = 12f;
+    float xRotationSpeed = 90f; 
+    float yRotationSpeed = 90f; 
+
+    float boostTime;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +25,22 @@ public class PlaneScript : MonoBehaviour
         float vAxis = Input.GetAxis("Vertical"); 
         float hAxis = Input.GetAxis("Horizontal"); 
 
-        
-        transform.Rotate(vAxis * xRotationSpeed, hAxis * yRotationSpeed, 0, Space.Self);
+        Vector3 amountToRotate = new Vector3(0, 0, 0);
+        amountToRotate.x = vAxis * xRotationSpeed;
+        amountToRotate.y = hAxis * yRotationSpeed;
+        amountToRotate *= Time.deltaTime;
+        transform.Rotate(amountToRotate, Space.Self);
 
-        
-        transform.position += transform.forward * forwardSpeed;
+    
+        transform.position += transform.forward * forwardSpeed * Time.deltaTime;
+
+        //Camera position
+        Vector3 cameraPosition = transform.position;
+        cameraPosition += -transform.forward * 10f;
+        cameraPosition += Vector3.up * 8f;
+        cameraObject.transform.position = cameraPosition;
+
+        cameraObject.transform.LookAt(transform.position);
     }
 
     // Detect collision with collectables
