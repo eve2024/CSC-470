fe7e5735a3ae1131de2a44bd300048421fe0ee35
eve2024 <144040969+eve2024@users.noreply.github.com>
2024-12-05 20:11:43 -0500

    added instuctions sheet

--- assigments/Final/Assets/GameManager.cs
+++ assigments/Final/Assets/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
 {
     public GameObject popUpWindow;
     public TMP_Text popUpText;
+    public GameObject instructions;
     // Start is called before the first frame update
    
     void Start()
@@ -24,5 +25,13 @@ public class GameManager : MonoBehaviour
     public void ClosePopUpWindow()
     {
         popUpWindow.SetActive(false);
+    }
+    public void ShowInstructions()
+    {
+        instructions.SetActive(true);
+    }
+    public void CloseInstructions()
+    {
+        instructions.SetActive(false);
     }
 }
